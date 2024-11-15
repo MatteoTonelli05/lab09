dfea33b25fe3f227c6ad0f2758e08271f4b1ac95
MatteoTonelli05 <matteo.tonelli8@studio.unibo.it>
2024-11-15 15:29:26 +0100

    controller is done

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,28 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private File currentFile;
+
+    public Controller(File currentFile) {
+        setCurrentFile(currentFile);
+    }
+
+    public Controller() {
+        this(new File(System.getProperty("user.home") + System.getProperty("file.separator") + "output.txt"));
+    }
+
+    public void setCurrentFile(File currentFile) {
+        this.currentFile = currentFile;
+    }
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
 
 }
